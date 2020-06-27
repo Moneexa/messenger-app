2345fbbb58809cf8656b67bc18b8f2f592446237
mohsini172 <mohsini172@gmail.com>
2020-06-27 14:10:25 +0500

    server addition

--- client/src/pages/conversionHistory/conversionHistory.jsx
+++ client/src/pages/conversionHistory/conversionHistory.jsx
@@ -1,23 +1,70 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './conversionHistory.css';
-import { Table } from 'react-bootstrap'
+import { Row, Container } from 'react-bootstrap'
 export default function converterHistory() {
 
 
-    return (<div className="converter-history">
-        <div className="pickar-logo-white">
-            <img className="ml-4 mt-4" src="../../pickar_logo_white.png" />
-        </div>
-        <div className="navigate-back">
-            <Link className="ml-4" to="/converter"> Go back </Link>
-        </div>
-        <div className="content">
-            <div className="container">
-                
-                
+    return (
+        <div className="converter-history">
+            <div className="pickar-logo-white">
+                <img className="ml-4 mt-4" src="../../pickar_logo_white.png" />
             </div>
-        </div>
-    </div>)
+            <div className="navigate-back">
+                <Link style={{
+                    color: "#FFF",
+                    fontSize: "12px",
+                    lineHeight: "14px"
+                }}
+                    className="ml-4" to="/converter"> Go back </Link>
+            </div>
+            <div className="content">
+                <Row>
+                    <Container>
+                        <div class="d-flex flex-flow-row-wrap mb-4">
+                            <p style={{
+                                opacity: "0.5",
+                                color: "#FFF",
+                                fontSize: "12px",
+                                lineHeight: "14px"
+                            }} className="col-md-4 col-sm-4">Date</p>
+                            <p style={{
+                                opacity: "0.5",
+                                color: "#FFF",
+                                fontSize: "12px",
+                                lineHeight: "14px"
+                            }} className="col-md-4 col-sm-4">From</p>
+                            <p style={{
+                                opacity: "0.5",
+                                color: "#FFF",
+                                fontSize: "12px",
+                                lineHeight: "14px"
+                            }} className="col-md-4 col-sm-4">To</p>
+                        </div>
+                    </Container>
+                </Row>
+                <Row>
+                    <Container>
+                        <div class="d-flex flex-flow-row-wrap my-2" style=
+                            {{ backgroundColor: "white", borderRadius: "10px", height: "59px" }}>
+                            <p style={{ color: "black" }} className="col-md-4 col-sm-4 px-4 py-2">Date</p>
+                            <p style={{ color: "black" }} className="col-md-4 col-sm-4 px-4 py-2">From</p>
+                            <p style={{ color: "black" }} className="col-md-4 col-sm-4 px-4 py-2">To</p>
+                        </div>
+                    </Container>
+                </Row>
+                <Row>
+                    <Container>
+                        <div class="d-flex flex-flow-row-wrap my-2" style=
+                            {{ backgroundColor: "white", borderRadius: "10px", height: "59px" }}>
+                            <p style={{ color: "black" }} className="col-md-4 col-sm-4 px-4 py-2">Date</p>
+                            <p style={{ color: "black" }} className="col-md-4 col-sm-4 px-4 py-2">From</p>
+                            <p style={{ color: "black" }} className="col-md-4 col-sm-4 px-4 py-2">To</p>
+                        </div>
+                    </Container>
+                </Row>
+            </div>
+
+        </div >)
 
 }
--- client/src/pages/converter/converter.jsx
+++ client/src/pages/converter/converter.jsx
@@ -1,151 +1,118 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Row, Container, Button } from 'react-bootstrap'
-import axios from 'axios'
 import makeRequest from '../../shared/service/currencyConverter.service';
 import { Link, withRouter } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faExchangeAlt } from '@fortawesome/free-solid-svg-icons'
 import './converter.css'
-export default class Converter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputValue: 0,
-            inputToCurrency: "EUR",
-            inputFromCurrency: "EUR",
-            outputValue: "",
-            result: "", result2: ""
-        }
-    }
-    handleValueOnChange = (e) => {
-        this.setState({
-            inputValue: e.target.value
-        })
-    }
-    handleToInputOnChange = (e) => {
-        console.log(e.target.value)
-        this.setState({
-            inputToCurrency: e.target.value
-        })
-    }
-    handleFromInputOnChange = (e) => {
-        console.log(e.target.value)
-        this.setState({
-            inputFromCurrency: e.target.value
-        })
-    }
-    handleSwap = (event) => {
-        const from = this.state.inputToCurrency;
-        const to = this.state.inputFromCurrency;
-        this.setState({
-            inputFromCurrency: from,
-            inputToCurrency: to
-        },
-            () => {
-                console.log(this.state.inputToCurrency)
+export default function Converter () {
 
-            }
-        )
-        this.handleConversion(event);
-        console.log(this.state)
+    const [inputValue, setInputValue] = useState(0);
+    const [inputToCurrency, setInputToCurrency] = useState("EUR");
+    const [inputFromCurrency, setInputFromCurrency] = useState("EUR");
+    const [outputValue, setOutputValue] = useState(0);
+    const [result, setResult] = useState("");
+    const [result2, setResult1] = useState("");
+
+    function handleValueOnChange(e) {
+        setInputValue(e.target.value)
     }
-    handleConversion = (e) => {
+    function handleToInputOnChange(e) {
+        setInputToCurrency(e.target.value)
+    }
+    function handleFromInputOnChange(e) {
+        setInputFromCurrency(e.target.value)
+    }
+    function handleSwap(event) {
+        const from = inputToCurrency;
+        const to = inputFromCurrency;
+        setInputToCurrency(to);
+        setInputFromCurrency(from);
+        handleConversion(event);
+    }
+    
+    function handleConversion(e) {
         e.preventDefault();
-        const from = this.state.inputFromCurrency;
-        const to = this.state.inputToCurrency;
-        try {
-            makeRequest(from, to)
-                .then((result) => {
-                    const value = Object.keys(result.data)
-                    const answer = (result.data[value]) * (this.state.inputValue)
-                    console.log(answer)
-                    this.setState({
-                        outputValue: answer
-                    }, () => {
-                        console.log(this.state)
-                        this.setState({
-                            result: this.state.inputValue + " " + this.state.inputFromCurrency + "=",
-                            result2: this.state.outputValue + " " + this.state.inputToCurrency
-                        })
-                    })
-                })
+        const from = inputFromCurrency;
+        const to = inputToCurrency;
+        console.log(outputValue)
+        setResult(inputValue+" "+from+" "+ "=");
+        setResult1(outputValue + " " + to);
 
-        }
-        catch (error) {
-            console.log(error)
-        }
 
-        console.log(this.state)
 
         // res.then(resp=>console.log(resp))
     }
-    render() {
-        return (
-            <div className="converter">
-                <div className="background-upper-half">
-                    <div className="pickar-logo-white ml-4 mt-4">
-                        <img src="../../pickar_logo_white.png" />
-                    </div>
-                    <div className="d-flex col-sm-12 col-md-12" style={{ height: "50vh", position: "relative" }}>
-                        <p className="headline ml-4" >
-                            Convert currencies in real-time
+    return (
+        <div className="converter">
+            <div className="background-upper-half">
+                <div className="pickar-logo-white ml-4 mt-4">
+                    <img src="../../pickar_logo_white.png" />
+                </div>
+                <div className="d-flex col-sm-12 col-md-12" style={{ height: "50vh", position: "relative" }}>
+                    <p className="headline ml-4" >
+                        Convert currencies in real-time
                     </p>
-                    </div>
                 </div>
-                <div className="container over-parent">
-                    <div className="parent-div">
-                        <div className="p-4 col-md-8 col-sm-12 form-div" style={{
-                            borderRadius: "5%", backgroundColor: "white",
-                            boxShadow: "0 41.8px 33.4px rgba(0, 0, 0, 0.086)"
-                        }}>
-                            <Container>
-                                <Form className="d-flex flex-flow-row-wrap align-items-center" onSubmit={this.handleConversion}>
-                                    <Form.Group controlId="inputValue" className="col-md-3 col-sm-12">
-                                        <Form.Label>Amount</Form.Label>
-                                        <Form.Control onChange={this.handleValueOnChange}
-                                            type="number" value={this.state.inputValue} placeholder="1.00" />
-                                    </Form.Group>
-                                    <Form.Group controlId="inputFromCurrency" className="col-md-3 col-sm-12">
-                                        <Form.Label>From</Form.Label>
-                                        <Form.Control as="select" value={this.state.inputFromCurrency}
-                                            onChange={this.handleFromInputOnChange}>
-                                            <option>EUR</option>
-                                            <option>USD</option>
-                                            <option>CHF</option>
-                                        </Form.Control>
-                                    </Form.Group>
-                                    <FontAwesomeIcon onClick={this.handleSwap} style={{ color: "red" }} icon={faExchangeAlt} />
-                                    <Form.Group controlId="inputToCurrency" className="col-md-3 col-sm-12">
-                                        <Form.Label>To</Form.Label>
-                                        <Form.Control as="select" value={this.state.inputToCurrency}
-                                            onChange={this.handleToInputOnChange}>
-                                            <option>EUR</option>
-                                            <option>USD</option>
-                                            <option>CHF</option>
-                                        </Form.Control>
-                                    </Form.Group>
-                                    <button type="submit" className="btn btn-danger col-md-3 col-sm-3">Convert</button>
-                                </Form>
-                            </Container>
-                        </div>
-                        <div className="col-md-4 col-sm-12" >
-                            <Link className="history" to="/conversion-history">View conversion history></Link>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="calculator">
-                    <div className="d-flex align-items-center justify-content-center">
-                        <div className="container">
-                            <p className="input"> 1 EUR={this.state.result}</p>
-                            <br />
-                            <p className="output">0.10 USD{this.state.result2}</p>
-                        </div>
-                    </div>
-                </div>
-
             </div>
-        )
+            <div className="container over-parent">
+                <div className="parent-div">
+                    <div className="p-4 col-md-8 col-sm-12 form-div" style={{
+                        borderRadius: "10px", backgroundColor: "white",
+                        boxShadow: "0 41.8px 33.4px rgba(0, 0, 0, 0.086)"
+                    }}>
+                        <Container>
+                            <Form className="d-flex flex-flow-row-wrap align-items-center" onSubmit={handleConversion}>
+                                <Form.Group controlId="inputValue" className="col-md-3 col-sm-12">
+                                    <Form.Label>Amount</Form.Label>
+                                    <Form.Control onChange={handleValueOnChange}
+                                        type="number" value={inputValue} placeholder="1.00" />
+                                </Form.Group>
+                                <Form.Group controlId="inputFromCurrency" className="col-md-3 col-sm-12">
+                                    <Form.Label>From</Form.Label>
+                                    <Form.Control as="select" value={inputFromCurrency}
+                                        onChange={handleFromInputOnChange}>
+                                        <option>EUR</option>
+                                        <option>USD</option>
+                                        <option>CHF</option>
+                                    </Form.Control>
+                                </Form.Group>
+                                <FontAwesomeIcon onClick={handleSwap} style={{ color: "red" }} icon={faExchangeAlt} />
+                                <Form.Group controlId="inputToCurrency" className="col-md-3 col-sm-12">
+                                    <Form.Label>To</Form.Label>
+                                    <Form.Control as="select" value={inputToCurrency}
+                                        onChange={handleToInputOnChange}>
+                                        <option>EUR</option>
+                                        <option>USD</option>
+                                        <option>CHF</option>
+                                    </Form.Control>
+                                </Form.Group>
+                                <button type="submit" className="btn btn-danger col-md-3 col-sm-3">Convert</button>
+                            </Form>
+                        </Container>
+                    </div>
+                    <div className="col-md-4 col-sm-12" >
+                        <Link style={{
+                            color: "#363A3E",
+                            fontSize: "12px",
+                            lineHeight: "14px"
+                        }} className="history" to="/conversion-history">View conversion history></Link>
+                    </div>
+                </div>
+            </div>
 
-    }
+            <div className="calculator">
+                <div className="d-flex align-items-center justify-content-center">
+                    <div className="container">
+                        <p className="input"> {result}</p>
+                        <br />
+                        <p className="output">{result2}</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+
+
 }
